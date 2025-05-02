0d623dd40c33ddf0d303100d1712c61e37d6e5c9
Clelton <cleltonjm_95@hotmail.com>
2025-05-02 11:25:03 -0300

    feat: implementing registry_updater with registry_updater_validator

--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -45,11 +45,11 @@ class OrdersRepository(OrdersRepositoryInterface):
         data = collection.find_one({"_id": ObjectId(object_id)})
         return data
 
-    def edit_registry(self) -> None:
+    def edit_registry(self, order_id: str, update_fields: dict) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.update_one(
-            {"_id": ObjectId("68124f426b3f2b16cdabd51d")}, # Filtros
-            {"$set": {"itens.pizza.quantidade": 30}}, # Edição
+            {"_id": ObjectId(order_id)}, # Filtros
+            {"$set": update_fields}, # Edição
         )
 
     def edit_many_registries(self) -> None:
